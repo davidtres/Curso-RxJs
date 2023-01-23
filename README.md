a64a29fcf3b98e69d5caf14c9a3f95a4b36a6ca2
David Trespalacios <gerencia@hardsoftcomputers.com.co>
2023-01-23 20:54:37 +0100

    Aplicación de fromEvent

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Curso-RxJs
+
 Practicas del curso Curso de Programación Reactiva con RxJS (Platzi)
+
+Crearemos para este curso una replica del juego Wordle, usando como base RxJs.
+
+1. Usaremos **fromEvent** para escuchar los eventos del teclado y filtrar las letras recibidas. Luego las encajamos en cada
+   DIV de la grilla y se resulelve reto para eliminar la ultima letra.
--- src/index.js
+++ src/index.js
@@ -1,24 +1,50 @@
-import { Observable } from "rxjs";
-import { Subscriber } from "rxjs/internal/Subscriber";
+import { Observable, fromEvent } from "rxjs";
 
-const onservableAlfa$ = new Observable(subscriber=>{
-    subscriber.next(1);
-    subscriber.next(2);
-    subscriber.complete()
-    subscriber.next(13);
-    subscriber.next(a=b);
-    subscriber.next('Mi primer observador');
-    subscriber.next({test:true});
-})
-
-const observador = {
-    next:(value)=>{
-        console.log(value);
-    },
-    complete:()=>{console.log('Observer Completado');},
-    error:(err)=>{
-        console.error(err);
+const onKeyDown$ = fromEvent(document, "keydown");
+const letterRows = document.getElementsByClassName("letter-row");
+let letterIndex = 0;
+let letterRowIndex = 0;
+const insertLetter = {
+  next: (event) => {
+    const pressedKey = event.key.toUpperCase();
+    const BACK = event.keyCode === 8 ?? event.key;
+    if (BACK) {
+      letterIndex--;
+      return backLetter(letterRows);
     }
+    if (pressedKey.length === 1 && pressedKey.match(/[a-z]/i)) {
+      let letterBox = letterRows[letterRowIndex].children[letterIndex];
+      letterBox.textContent = pressedKey;
+      letterBox.classList.add("filled-letter");
+      letterIndex++;
+    }
+  },
+};
+// Borrar letra de la misma fila, segun reto del curso.
+function backLetter(letterRows) {
+  let letterBox = letterRows[letterRowIndex].children[letterIndex];
+  letterBox.textContent = "";
+  letterBox.classList.remove("filled-letter");
 }
+onKeyDown$.subscribe(insertLetter);
 
-onservableAlfa$.subscribe(observador)
+// Primer ejecicio
+// const onservableAlfa$ = new Observable(subscriber=>{
+//     subscriber.next(1);
+//     subscriber.next(2);
+//     subscriber.complete()
+//     subscriber.next(13);
+//     subscriber.next(a=b);
+//     subscriber.next('Mi primer observador');
+//     subscriber.next({test:true});
+// })
+// const observador = {
+//     next:(value)=>{
+//         console.log(value);
+//     },
+//     complete:()=>{console.log('Observer Completado');},
+//     error:(err)=>{
+//         console.error(err);
+//     }
+// }
+// onservableAlfa$.subscribe(observador)
