f9660457bb22bd9a4b0a659ade5bdd3b556f3363
David Trespalacios <gerencia@hardsoftcomputers.com.co>
2023-01-25 11:43:09 +0100

    Uso de Subject()

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Crearemos para este curso una replica del juego Wordle, usando como base RxJs.
 
 1. Usaremos **fromEvent** para escuchar los eventos del teclado y filtrar las letras recibidas. Luego las encajamos en cada
    DIV de la grilla y se resulelve reto para eliminar la ultima letra.
+2. **Subject()** Nos ayuda para disparar un evento next() luego de verificar si el usuario a ganado o perdido al presionar tecla "Enter" para colorear las letras correctas aplicando una clase css.
--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import { Observable, Subject, fromEvent } from "rxjs";
+import WORD_LIST from "./wordsList.json";
 
 const onKeyDown$ = fromEvent(document, "keydown");
 const letterRows = document.getElementsByClassName("letter-row");
 let letterIndex = 0;
 let letterRowIndex = 0;
+let userAnswer = [];
+
+const getRandomWord = () =>
+  WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)];
+// Obtener palabra aleatoria
+const rightWord = getRandomWord();
+console.log(rightWord);
+
+const userWinOrLoose$ = new Subject();
+
 const insertLetter = {
   next: (event) => {
     const pressedKey = event.key.toUpperCase();
     const BACK = event.keyCode === 8 ?? event.key;
     if (BACK && letterIndex > 0) {
       letterIndex--;
+      userAnswer.pop();
       return backLetter(letterRows);
     }
     if (pressedKey.length === 1 && pressedKey.match(/[a-z]/i)) {
@@ -17,6 +29,7 @@ const insertLetter = {
       letterBox.textContent = pressedKey;
       letterBox.classList.add("filled-letter");
       letterIndex++;
+      userAnswer.push(pressedKey);
     }
   },
 };
@@ -26,8 +39,32 @@ function backLetter(letterRows) {
   letterBox.textContent = "";
   letterBox.classList.remove("filled-letter");
 }
+//observador para verificar si la palabra ingresada es correcta.
+const checkWord = {
+  next: (event) => {
+    if (event.key === "Enter") {
+      const wordWrite = userAnswer.join("");
+      const wordCorrect = wordWrite === rightWord ?? false;
+      if (wordCorrect) {
+        const result = wordCorrect ? "Has Ganado!!" : "Has Perdido";
+        const mensaje = document.getElementsByClassName("messaje-text")[0];
+        mensaje.innerHTML = result;
+        mensaje.classList.add("letter-green");
+        console.log(wordCorrect);
+        userWinOrLoose$.next();
+      }
+    }
+  },
+};
 onKeyDown$.subscribe(insertLetter);
-
+onKeyDown$.subscribe(checkWord);
+userWinOrLoose$.subscribe(() => {
+  const letterRowsWinned = Array.from(letterRows)[letterRowIndex];
+  console.log(letterRowsWinned);
+  for (let i = 0; i < 5; i++) {
+    letterRowsWinned.children[i].classList.add("letter-green");
+  }
+});
 // Primer ejecicio para entender funcionamiento de Observable
 // const onservableAlfa$ = new Observable(subscriber=>{
 //     subscriber.next(1);
@@ -52,26 +89,26 @@ onKeyDown$.subscribe(insertLetter);
 //Ejercicio para entender Observables: Subject
 //Clase https://platzi.com/clases/3233-programacion-reactiva-rxjs/50758-observables-subject/
 
-const numbers$ = new Observable((subscriber) => {
-  subscriber.next(Math.round(Math.random() * 100));
-});
+// const numbers$ = new Observable((subscriber) => {
+//   subscriber.next(Math.round(Math.random() * 100));
+// });
 
-const numbersRandom$ = new Subject();
+// const numbersRandom$ = new Subject();
 
-const observador1 = {
-  next: (number) => {
-    console.log(number);
-  },
-};
-const observador2 = {
-  next: (number) => {
-    console.log(number);
-  },
-};
+// const observador1 = {
+//   next: (number) => {
+//     console.log(number);
+//   },
+// };
+// const observador2 = {
+//   next: (number) => {
+//     console.log(number);
+//   },
+// };
 
-numbersRandom$.subscribe(observador1);
-numbersRandom$.subscribe(observador2);
+// numbersRandom$.subscribe(observador1);
+// numbersRandom$.subscribe(observador2);
 
-numbers$.subscribe(numbersRandom$);
+// numbers$.subscribe(numbersRandom$);
 
-numbersRandom$.next(45);
+// numbersRandom$.next(45);
